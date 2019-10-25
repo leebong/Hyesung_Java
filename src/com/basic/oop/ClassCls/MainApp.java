15b7b6e3a917fe762f3a26793ed804c97b63b7f9
HS8 <HS8@DESKTOP-DIF1TVL>
2019-10-25 20:39:13 +0900

    2019-10-25-02

--- src/com/basic/oop/ClassCls/MainApp.java
+++ src/com/basic/oop/ClassCls/MainApp.java
@@ -4,7 +4,7 @@ package com.basic.oop.ClassCls;
  *  Class 클래스의 역할 다른 클래스 정보를 저장
  *  멤버, 메서드 생성자
  */
-
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class MainApp {
@@ -14,14 +14,11 @@ public class MainApp {
 		
 		// Exception --> 모든 예외의 조상
 		Class uCls = User.class;
-		Contructor cs = uCls.getConstructor(new Class[] {String.class, String.class});
+		
+		Constructor cs = uCls.getConstructor(new Class[] {String.class, String.class});
 		User admin = (User) cs.newInstance("admin", "1q2w3e4r!");
 		Method method = uCls.getMethod("toString");
 		System.out.println(method.invoke(admin));
 		
-		
-		
-		
-	
 
 }
